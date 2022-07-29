2364f56f4a06277ebf73c373406011c78e0f562d
DIogo Alpino <diogoalpino@gmail.com>
2022-07-29 18:27:05 -0300

    Exercicio 1

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-events/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-events/script.js
@@ -14,3 +14,20 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  function createDays () {
+    let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    let daysList = document.querySelector('#days');
+
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+      let dias = decemberDaysList[index];
+      let listaDosDias = document.createElement('li');
+      listaDosDias.innerHTML = dias;
+
+      daysList.appendChild(listaDosDias);
+    };
+  };
+
+  createDays();
+
+  //Exercicio 2
