d93e72ec8e9f0e7abe779e9284db4143988ca1af
DIogo Alpino <diogoalpino@gmail.com>
2022-08-01 19:08:09 -0300

    Font-Size

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-dom-eventos-e-web-storage/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-dom-eventos-e-web-storage/script.js
@@ -3,6 +3,9 @@ let select = document.querySelector("select");
 select.addEventListener("change", function(){
     let selected = select.selectedOptions[0];
     document.body.style.backgroundColor = selected.value;
+
+    localStorage.setItem("5.4-backgroundColor", selected.value);
+
 });
 
 let select2 = document.getElementById("select2");
@@ -10,4 +13,19 @@ select2.addEventListener("change", function(){
     let selected2 = select2.selectedOptions[0];
     document.body.style.color = selected2.value;
 });
+
+let inputFontSize = document.querySelector('input[name = "font-size"]');
+inputFontSize.addEventListener("change", function(){
+    let p = document.querySelector("p");
+    p.style.fontSize = `${inputFontSize.value}px`; 
+
+    localStorage.setItem("5.4-fontSize", `${inputFontSize.value}px`);
+});
+
+let savedBackground = localStorage.getItem("5.4-backgroundColor");
+document.body.style.backgroundColor = savedBackground;
+
+let savedFontSize = localStorage.getItem("5.4-fontSize");
+let p = document.querySelector("p");
+p.style.fontSize = savedFontSize;
 }
