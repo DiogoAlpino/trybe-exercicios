f6198670c86d798b2fd83e9ca6db02ddc9b5b1e0
DIogo Alpino <diogoalpino@gmail.com>
2022-08-15 19:42:42 -0300

    Exercicios Pt III

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/Parte III/Exercicios.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/Parte III/Exercicios.js
@@ -55,4 +55,45 @@ const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 
 //console.log(allLessons);
 
+//6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const students = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+
+  for (let index = 0; index < array.length; index += 1) {
+    total = total + obj[array[index]].numeroEstudantes;
+  }
+  return total;
+};
+
+//console.log(students(allLessons));
+
+//7 - Crie uma função que obtenha o valor da chave de acordo com o seu índice no objeto. 
+
+const getKeyValue = (obj, number) => Object.values(obj)[number];
+
+//console.log(getKeyValue(lesson2, 1));
+
+//8 - Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles:
+// o objeto, o nome da chave e o valor da chave. 
+
+const verify = (obj, key, value) => {
+  const array = Object.entries(obj);
+  let verifica = false;
+
+  for (let index = 0; index < array.length; index += 1) {
+
+    if (array[index][0] === key && array[index][1] === value)
+    verifica = true;
+  }
+  return verifica;
+
+};
+
+console.log(verify(lesson2, 'professor', 'Carlos'));
+
+
+
+
 
